3682f1162a2bb2ffb97cc243388368892f51227f
Allen <54394182+PrintName@users.noreply.github.com>
2019-08-26 05:01:10 -0400

    Initial commit
    Week 1 video lesson work

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Allen Li on 8/26/19.
+//  Copyright © 2019 Allen Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
